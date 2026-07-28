9bee6f3c5b461ec846b17230d44394c13fa90689
Caio Lima <caiodelima@klever.io>
2026-07-28 18:52:48 -0300

    chore: configure Retrofit network setup

--- app/src/main/java/com/example/customerchallenge/di/App.kt
+++ app/src/main/java/com/example/customerchallenge/di/App.kt
@@ -11,7 +11,7 @@ class App : Application() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules(appModule)
+            modules(appModule,netWorkModule)
         }
     }
 }
