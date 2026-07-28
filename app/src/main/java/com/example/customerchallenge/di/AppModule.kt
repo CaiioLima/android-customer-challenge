e94914de86e7ad9e9547276f72c93869796dc693
Caio Lima <caiooalexandre94@gmail.com>
2026-07-28 20:30:29 -0300

    feat: add get customers use case

--- app/src/main/java/com/example/customerchallenge/di/AppModule.kt
+++ app/src/main/java/com/example/customerchallenge/di/AppModule.kt
@@ -3,14 +3,18 @@ package com.example.customerchallenge.di
 import com.example.customerchallenge.data.remote.datasource.CustomerRemoteDataSource
 import com.example.customerchallenge.data.repository.CustomerRepositoryImpl
 import com.example.customerchallenge.domain.repository.CustomerRepository
+import com.example.customerchallenge.domain.usecase.GetCustomersUseCase
 import org.koin.dsl.module
 
 val appModule = module {
 
     //remoteDataSource
-    single{ CustomerRemoteDataSource(api = get()) }
+    single { CustomerRemoteDataSource(api = get()) }
 
     //repository
-    single<CustomerRepository>{ CustomerRepositoryImpl(get()) }
+    single<CustomerRepository> { CustomerRepositoryImpl(get()) }
+
+    //usecase
+    factory { GetCustomersUseCase(repository = get()) }
 
 }
