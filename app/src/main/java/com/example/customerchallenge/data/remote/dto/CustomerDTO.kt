2ecff3f945935d59cc4abf801e7d7877bc88efb5
Caio Lima <caiooalexandre94@gmail.com>
2026-07-28 19:58:05 -0300

    feat: configure customer API

---
+++ app/src/main/java/com/example/customerchallenge/data/remote/dto/CustomerDTO.kt
@@ -0,0 +1,26 @@
+package com.example.customerchallenge.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CustomerDTO(
+    @SerializedName("id")
+    val id: String?,
+
+    @SerializedName("name")
+    val name: String?,
+
+    @SerializedName("status")
+    val status: String?,
+
+    @SerializedName("email")
+    val email: String?,
+
+    @SerializedName("phone")
+    val phone: String?,
+
+    @SerializedName("profileImage")
+    val profileImage: String?,
+
+    @SerializedName("profileLink")
+    val profileLink: String?
+)
