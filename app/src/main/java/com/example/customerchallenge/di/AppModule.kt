eb390d6f91d5be0b030560436d85c6a4a99bda16
Caio Lima <caiooalexandre94@gmail.com>
2026-07-28 20:16:55 -0300

    feat: add customer remote data source

--- app/src/main/java/com/example/customerchallenge/di/AppModule.kt
+++ app/src/main/java/com/example/customerchallenge/di/AppModule.kt
@@ -1,7 +1,11 @@
 package com.example.customerchallenge.di
 
+import com.example.customerchallenge.data.remote.datasource.CustomerRemoteDataSource
 import org.koin.dsl.module
 
 val appModule = module {
+
+    //remoteDataSource
+    single{ CustomerRemoteDataSource(api = get()) }
 
 }
